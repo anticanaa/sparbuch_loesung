de9de110628383c92eb6bf454a0e0e010941e572
Ana Antic <aantic@schueler.hakzell.at>
2024-01-31 11:26:41 +0100

    add class

---
+++ Sparbuch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sparbuch_loesung;
+
+public class Sparbuch
+{
+
+}
