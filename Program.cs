17c431364388dc0f8e54c0be3e2e8458bf7d5828
Ana Antic <aantic@schueler.hakzell.at>
2024-02-09 13:00:31 +0100

    update program

--- Program.cs
+++ Program.cs
@@ -2,3 +2,10 @@
 
 Sparbuch s1 = new("Hans Maier");
 
+s1.Einzahlen(20);
+s1.Einzahlen(30);
+s1.Einzahlen(40);
+
+Console.WriteLine("Es wurden {0} Beträge eingezahlt", s1.GetAnzahl());
+
+Console.WriteLine("Guthaben: {0}", s1.GetGuthaben());
